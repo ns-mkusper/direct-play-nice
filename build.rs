b8ff0ec5b0bce5c3d6df73535c0458c432bf3d0a
Mark Kusper <33031985+ns-mkusper@users.noreply.github.com>
2026-04-29 12:53:56 -0500

    Fix release runner labels (#99)

---
+++ build.rs
@@ -0,0 +1,23 @@
+fn main() {
+    if std::env::var("CARGO_CFG_TARGET_VENDOR").as_deref() == Ok("apple") {
+        for framework in [
+            "AudioToolbox",
+            "CoreFoundation",
+            "CoreMedia",
+            "CoreVideo",
+            "Security",
+            "VideoToolbox",
+        ] {
+            println!("cargo:rustc-link-lib=framework={framework}");
+        }
+    }
+
+    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
+        for lib in [
+            "Mfplat", "Strmiids", "Mfuuid", "Bcrypt", "Ncrypt", "Crypt32", "Secur32", "Ole32",
+            "User32",
+        ] {
+            println!("cargo:rustc-link-lib={lib}");
+        }
+    }
+}
