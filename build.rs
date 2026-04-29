a2e27f6e5f5b303919b53986f34c7f73320f0e75
Mark Kusper <33031985+ns-mkusper@users.noreply.github.com>
2026-04-29 15:53:15 +0000

    build: link windows ffmpeg system libs

--- build.rs
+++ build.rs
@@ -11,4 +11,13 @@ fn main() {
             println!("cargo:rustc-link-lib=framework={framework}");
         }
     }
+
+    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
+        for lib in [
+            "Mfplat", "Strmiids", "Mfuuid", "Bcrypt", "Ncrypt", "Crypt32", "Secur32", "Ole32",
+            "User32",
+        ] {
+            println!("cargo:rustc-link-lib={lib}");
+        }
+    }
 }
